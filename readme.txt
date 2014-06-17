6b7e309f6753398de3efd5890a59eabcfebbd28b
wp.insider <wp.insider@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-17 02:55:44 +0000

    committing v1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-membership-after-login-redirection/trunk@933416 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Simple Membership After Login Redirection ===
+Contributors: smp7, wp.insider
+Donate link: https://simple-membership-plugin.com/
+Tags: login, redirection, member, members, membership, login redirection, access, level
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv2 or later
+
+An addon for the simple membership plugin to configure after login redirection to a specific page based on the member's level.
+
+== Description ==
+
+This addon allows you to configure an after login page for each of the membership access levels you create in your [simple membership plugin](http://wordpress.org/plugins/simple-membership/).
+
+This addon plugin will automatically redirect the members to the appropriate page after they log into your site.
+
+After you install this addon, edit your membership levels and specify the redirect pages and the addon will take care of the rest.
+
+== Installation ==
+
+1. Upload `swpm-after-login-redirection` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+None
+
+== Screenshots ==
+
+None
+
+== Changelog ==
+
+= 1.0 =
+* First commit to wordpress.org
+
+== Upgrade Notice ==
+
+None
---
+++ swpm-after-login-redirection.php
@@ -0,0 +1,98 @@
+<?php
+/*
+  Plugin Name: Simple Membership After Login Redirection
+  Version: v1.0
+  Plugin URI: https://simple-membership-plugin.com/
+  Author: smp7, wp.insider
+  Author URI: https://simple-membership-plugin.com/
+  Description: An addon for the simple membership plugin to do the after login redirection to a specific page based on the member's level.
+*/
+
+if (!defined('ABSPATH'))
+    exit; //Exit if accessed directly
+
+define('SWPM_ALR_CONTEXT', 'swpm_alr');
+
+add_action('init', 'swpm_alr_addon_init');
+add_action('swpm_after_login', 'swpm_alr_do_after_login_redirection');
+
+function swpm_alr_addon_init() {
+    if (!class_exists('SimpleWpMembership')) {
+        return;
+    }
+
+    if (is_admin()) {//Do admin side stuff
+        add_filter('swpm_admin_add_membership_level_ui', 'swpm_alr_admin_add_membership_level_ui');
+        add_filter('swpm_admin_edit_membership_level_ui', 'swpm_alr_admin_edit_membership_level_ui', 10, 2);
+
+        add_filter('swpm_admin_add_membership_level', 'swpm_alr_admin_add_membership_level');
+        add_filter('swpm_admin_edit_membership_level', 'swpm_alr_admin_edit_membership_level', 10, 2);
+    } else {//Do front end stuff
+    }
+}
+
+function swpm_alr_admin_add_membership_level_ui($to_filter) {
+    return $to_filter . '<tr>
+            <th scope="row">After Login Redirection Page</th>
+            <td>
+            <input type="text" class="regular-text" name="custom[swpm_alr_after_login_page_field]" value="" />
+            <p class="description">Enter the URL of the page where you want members of this level to be redirected to after they login.</p>
+            </td>
+            </tr>';
+}
+
+function swpm_alr_admin_edit_membership_level_ui($to_filter, $id) {
+    $fields = BMembershipLevelCustom::get_value_by_context($id, SWPM_ALR_CONTEXT);
+    $swpm_alr_after_login_page_field = isset($fields['swpm_alr_after_login_page_field']) ? $fields['swpm_alr_after_login_page_field']['meta_value'] : '';
+    return $to_filter . '<tr>
+            <th scope="row">After Login Redirection Page</th>
+            <td>
+            <input type="text" class="regular-text" name="custom[swpm_alr_after_login_page_field]" value="' . $swpm_alr_after_login_page_field . '" />
+            <p class="description">Enter the URL of the page where you want members of this level to be redirected to after they login.</p>
+            </td>
+            </tr>';
+}
+
+function swpm_alr_admin_add_membership_level($to_filter) {
+    $custom_field = $_POST['custom']['swpm_alr_after_login_page_field'];
+    $field = array(
+        'meta_key' => 'swpm_alr_after_login_page_field', // required
+        'meta_value' => sanitize_text_field($custom_field), //required
+        'meta_context' => SWPM_ALR_CONTEXT, // optional but recommended
+        'meta_label' => '', // optional
+        'meta_type' => 'text'// optional
+    );
+    $to_filter['swpm_alr_after_login_page_field'] = $field;
+    return $to_filter;
+}
+
+function swpm_alr_admin_edit_membership_level($to_filter, $id) {
+    $custom_field = $_POST['custom']['swpm_alr_after_login_page_field'];
+    $field = array(
+        'meta_key' => 'swpm_alr_after_login_page_field', // required
+        'meta_value' => sanitize_text_field($custom_field), //required
+        'meta_context' => SWPM_ALR_CONTEXT, // optional but recommended
+        'meta_label' => '', // optional
+        'meta_type' => 'text'// optional
+    );
+    $to_filter['swpm_alr_after_login_page_field'] = $field;
+    return $to_filter;
+}
+
+function swpm_alr_do_after_login_redirection() {
+    if(class_exists('BLog')){
+        Blog::log_simple_debug("After login redirection addon. Checking if member need to be redirected.",true);
+    }
+
+    $auth = BAuth::get_instance();
+    if ($auth->is_logged_in()) {
+        $level = $auth->get('membership_level');
+        $level_id = $level;
+        $key = 'swpm_alr_after_login_page_field';
+        $after_login_page_url = BMembershipLevelCustom::get_value_by_key($level_id, $key);
+        if(!empty($after_login_page_url)){
+            wp_redirect($after_login_page_url);
+            exit;
+        }
+    }
+}
